d702439ad9628a88a043e707389f0223d9e514c5
justyasen <ymanahov@gmail.com>
2022-05-30 12:40:11 +0300

    moved function to check permission to homescreen

--- src/screens/CameraScreen/CameraScreen.tsx
+++ src/screens/CameraScreen/CameraScreen.tsx
@@ -4,22 +4,12 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 import {Camera, useCameraDevices} from 'react-native-vision-camera';
 
 export const CameraScreen: React.FC = () => {
-  const checkCameraPermission = async () => {
-    let status = await Camera.getCameraPermissionStatus();
-    if (status !== 'authorized') {
-      await Camera.requestCameraPermission();
-      status = await Camera.getCameraPermissionStatus();
-      if (status === 'denied') {
-        Alert.alert(
-          'You will not be able to scan if you do not allow camera access',
-        );
-      }
-    }
-  };
+    return(
+      <></>
+    )
 
-  useEffect(() => {
-    checkCameraPermission();
-  }, []);
+};
+
 
   const devices = useCameraDevices();
   const device = devices.back;
--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,12 +1,30 @@
 import {useNavigation} from '@react-navigation/native';
-import React from 'react';
-import {SafeAreaView, Text, TouchableOpacity} from 'react-native';
+import React, {useEffect} from 'react';
+import {Alert, SafeAreaView, Text, TouchableOpacity} from 'react-native';
 import {CAMERA_ROUTE, GALLERY_ROUTE} from '../../routes';
 import {NavigationProps} from '../../types/NavigationProps';
+import {Camera} from 'react-native-vision-camera';
 
 export const HomeScreen: React.FC = () => {
   //router
   const navigation = useNavigation<NavigationProps>();
+
+  const checkCameraPermission = async () => {
+    let status = await Camera.getCameraPermissionStatus();
+    if (status !== 'authorized') {
+      await Camera.requestCameraPermission();
+      status = await Camera.getCameraPermissionStatus();
+      if (status === 'denied') {
+        Alert.alert(
+          'You will not be able to scan if you do not allow camera access',
+        );
+      }
+    }
+  };
+
+  useEffect(() => {
+    checkCameraPermission();
+  }, []);
 
   const navigateToCameraScreen = () => navigation.navigate(CAMERA_ROUTE);
   const navigateToGalleryScreen = () => navigation.navigate(GALLERY_ROUTE);
